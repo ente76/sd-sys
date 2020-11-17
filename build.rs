4fc056a33b3adc01680d83386204d78d9c755af0
ente <ente@ck76.de>
2020-11-17 13:23:59 +0100

    build.rs: fixed linking target to libsystemd

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() {
-    pkg_config::Config::new().probe("systemd").unwrap();
+    pkg_config::Config::new().probe("libsystemd").unwrap();
     println!("cargo:rerun-if-changed=build.rs");
 }
